02dc2400f7087525cddc30be264635c8d9fae397
Alexandr Limonov <alex-limonov@yandex-team.ru>
2022-08-22 11:47:42 +0300

    spirint2_increment5

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -16,7 +16,7 @@ func main() {
 	mux := handler.New(Storage)
 	mux.Use(middleware.RequestID)
 	mux.Use(middleware.RealIP)
-	mux.Use(middleware.Logger)
+	//mux.Use(middleware.Logger)
 	mux.Use(middleware.Recoverer)
 
 	mux.Post("/update/{metricType}/{metricName}/{metricValue}", mux.Update)
