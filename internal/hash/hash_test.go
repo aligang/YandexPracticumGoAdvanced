49aaf0502377642df370836d4583115eb6ba5ac1
Alexandr Limonov <alex-limonov@yandex-team.ru>
2022-09-08 02:00:58 +0300

    sprint3

--- internal/hash/hash_test.go
+++ internal/hash/hash_test.go
@@ -9,10 +9,10 @@ import (
 
 func TestHash(t *testing.T) {
 	t.Run("HASHING of GAUGE", func(t *testing.T) {
-		var refValue float64 = 3877
-		refMetric := metric.Metrics{ID: "BuckHashSys", MType: "gauge", Value: &refValue}
-		refHash := "93d036cf9d13e15e0829338347901f16c158998cdc8156e0f08977625f30a462"
-		refKey := "1"
+		var refValue float64 = 4194304.000000
+		refMetric := metric.Metrics{ID: "NextGC", MType: "gauge", Value: &refValue}
+		refHash := "54b80493a21162a59b7362fee7445c12c6cab4fb912b8262aeadff13781e73a1"
+		refKey := "/tmp/PHvif"
 		hash, _ := CalculateHash(&refMetric, refKey)
 		assert.Equal(t, true, hmac.Equal([]byte(refHash), []byte(hash)))
 	})
