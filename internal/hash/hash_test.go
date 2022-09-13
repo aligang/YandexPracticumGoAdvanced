6189d0cf116c9e970eba394fd47f7719436d3e93
Alexandr Limonov <alex-limonov@yandex-team.ru>
2022-09-13 23:38:44 +0300

    sprint3

--- internal/hash/hash_test.go
+++ internal/hash/hash_test.go
@@ -2,7 +2,6 @@ package hash
 
 import (
 	"crypto/hmac"
-	"fmt"
 	"github.com/aligang/YandexPracticumGoAdvanced/internal/metric"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -10,7 +9,7 @@ import (
 
 func TestHash(t *testing.T) {
 	t.Run("HASHING of GAUGE", func(t *testing.T) {
-		var refValue float64 = 4194304.000000
+		var refValue = 4194304.000000
 		refMetric := metric.Metrics{ID: "NextGC", MType: "gauge", Value: &refValue}
 		refHash := "54b80493a21162a59b7362fee7445c12c6cab4fb912b8262aeadff13781e73a1"
 		refKey := "/tmp/PHvif"
@@ -23,7 +22,6 @@ func TestHash(t *testing.T) {
 		refHash := "917f76e29b3cab53e3c9ea912567696501ec69bcbea708f89cf957a46b5b5012"
 		refKey := "/tmp/e6qMTm"
 		hash, _ := CalculateHash(&refMetric, refKey)
-		fmt.Printf(hash)
 		assert.Equal(t, true, hmac.Equal([]byte(refHash), []byte(hash)))
 	})
 }
